46aecdee3307531cb9af76cfb3bea82324c06dfa
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-05 13:40:47 +0530

    Create main.c

---
+++ Arduino Codes/Visible Spectroscopy/Visible Chip/main.c
@@ -0,0 +1,60 @@
+#include "SparkFun_AS7265X.h" 
+AS7265X sensor;
+ 
+void setup() {
+  Serial.begin(9600);
+  Serial.println("AS7265x Spectral Triad Example");
+ 
+  if(sensor.begin() == false)
+  {
+    Serial.println("Sensor does not appear to be connected. Please check the wiring. Freezing...");
+    while(1);
+  }
+  
+  Serial.println("A,B,C,D,E,F,G,H,I,J,K,L,R,S,T,U,V,W");
+}
+ 
+void loop() {
+  sensor.takeMeasurements(); //This is a hard wait while all 18 channels are measured
+ 
+  Serial.print(sensor.getCalibratedA());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedB());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedC());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedD());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedE());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedF());
+  Serial.print(",");
+ 
+  Serial.print(sensor.getCalibratedG());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedH());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedI());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedJ());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedK());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedL());
+  Serial.print(",");
+ 
+  Serial.print(sensor.getCalibratedR());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedS());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedT());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedU());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedV());
+  Serial.print(",");
+  Serial.print(sensor.getCalibratedW());
+  Serial.print(",");
+ 
+  Serial.println();
+}
