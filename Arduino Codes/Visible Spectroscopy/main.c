63e566a2024315d3865bad03d7321f82c42448e5
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-05 13:37:09 +0530

    Create main.c

---
+++ Arduino Codes/Visible Spectroscopy/main.c
@@ -0,0 +1,42 @@
+﻿#include "AS726X.h"
+
+
+AS726X sensor;
+
+
+void setup() 
+{
+  Wire.begin();
+  Serial.begin(115200);
+  sensor.begin();
+}
+
+
+void loop() 
+{
+  sensor.takeMeasurements();
+   if (sensor.getGreen() == SENSORTYPE_AS7262)
+  {
+    //Visible readings
+    Serial.print(" Reading: V[");
+    Serial.print(sensor.getCalibratedViolet(), 2);
+    Serial.print("] B[");
+    Serial.print(sensor.getCalibratedBlue(), 2);
+    Serial.print("] G[");
+    Serial.print(sensor.getCalibratedGreen(), 2);
+    Serial.print("] Y[");
+    Serial.print(sensor.getCalibratedYellow(), 2);
+    Serial.print("] O[");
+    Serial.print(sensor.getCalibratedOrange(), 2);
+    Serial.print("] R[");
+    Serial.print(sensor.getCalibratedRed(), 2);
+  }
+
+
+  Serial.print("] tempF[");
+  Serial.print(sensor.getTemperature(), 1);
+  Serial.print("]");
+
+
+  Serial.println();
+}
